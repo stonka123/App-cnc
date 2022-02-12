46001ae579cfd68668b62e3b3651c9bbd7e89c49
Tomasz <stonka123@gmail.com>
2022-02-12 12:01:56 +0100

    next : popup , darkmode

--- js/main.js
+++ js/main.js
@@ -1,14 +1,47 @@
-const kind = document.querySelector('.kind')
+const kind = document.querySelector('#kind')
 const diameter = document.querySelector('.diameter')
 const material = document.querySelector('.material')
 const Btn = document.querySelector('.button')
 const errorText = document.querySelector('.error-info')
-const score = document.querySelector('.spindle-rate')
-const options = document.querySelectorAll('.option')
+const scoreParagraph = document.querySelector('.spindle-rate')
+//KIND OPTION
+const option = document.querySelectorAll('.option1')
 
 const calculate = () => {
-	const box = 3.14 * parseFloat(diameter.value)
-	const drillHssScore = 25000 / box
-	score.textContent = `${drillHssScore.toFixed(0)}`
+	let boxScore = ((kind.value * 318) / diameter.value) * material.value
+	let boxe = (scoreParagraph.textContent = `${boxScore.toFixed(0)}`)
+	errorText.textContent = ''
+	// scoreParagraph.textContent = ''
+	scoreParagraph.style.visibility = 'visible'
+	checkString()
+	maxSpindle()
+	checkInf()
 }
+
+const checkInf = () => {
+	if (diameter.value === '') {
+		scoreParagraph.textContent = ''
+	} else if (diameter.value <= 0) {
+		errorText.style.visibility = 'visible'
+		errorText.textContent = 'Błąd średnicy narzędzia!'
+		scoreParagraph.textContent = ''
+	}
+}
+
+const checkString = () => {
+	const re = new RegExp('[^abc]')
+	if (diameter.value.match(re)) {
+	} else {
+		errorText.style.visibility = 'visible'
+		errorText.textContent = 'Błąd średnicy narzędzia!'
+		scoreParagraph.style.visibility = 'hidden'
+	}
+}
+
+const maxSpindle = () => {
+	if (scoreParagraph.textContent >= 20000) {
+		scoreParagraph.textContent = '20000'
+	}
+}
+
 Btn.addEventListener('click', calculate)
